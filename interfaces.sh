98310e828dc937b7c883dd06f8e8738025a63b2d
Noa Butterfield <noahbutterfield@learningstorm.org>
2026-01-06 14:17:37 -0800

    Refactor select_interface function for clarity

--- interfaces.sh
+++ interfaces.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 select_interface() {
-    mapfile -t IFACES < <(ip -o link show | awk -F': ' '{print $2}')
+    # Get list of interfaces
+    IFACES=()
+    while IFS=: read -r _ name _; do
+        IFACES+=("$name")
+    done < <(ip -o link show)
 
+    # Show menu
     echo "Select a network interface:"
     for i in "${!IFACES[@]}"; do
         printf "%d. %s\n" "$((i+1))" "${IFACES[$i]}"
     done
 
-    read -p "What interface? " CHOICE
+    # Read choice from terminal explicitly
+    read -p "What interface? " CHOICE </dev/tty
 
+    # Validate input
     if ! [[ "$CHOICE" =~ ^[0-9]+$ ]] || (( CHOICE < 1 || CHOICE > ${#IFACES[@]} )); then
         echo "Invalid choice."
         exit 1
     fi
 
+    # Export selected interface
     IFACE="${IFACES[$((CHOICE-1))]}"
     export IFACE
 }
+
+# Example usage
+select_interface
+echo "You selected: $IFACE"
